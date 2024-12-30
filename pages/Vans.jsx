f3171387a2ae3be94db66f1c8543b23e2fef17f6
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-30 10:01:12 -0500

    Route Params - 2

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,16 +1,10 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 
 /**
- * Challenge: Fetch and map over the data to display it on
- * the vans page. For an extra challenge, spend time styling
- * it to look like the Figma design.
- * 
- * Hints:
- * 1. Use `fetch("/api/vans")` to kick off the request to get the
- *    data from our fake Mirage JS server
- * 2. What React hook would you use to fetch data as soon as the
- *    Vans page loads, and only fetch it the one time?
+ * Challenge: Wrap the contents of the "van-tile" div in a 
+ * Link that sends the user to `/vans/${van-id-here}`.
  */
 
 export default function Vans() {
@@ -25,12 +19,14 @@ export default function Vans() {
 
     const vanElements = vans.map((van) => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 
