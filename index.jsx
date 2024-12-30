45c8a072e09343235bd8b2c96585b9d6a4600941
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-30 12:01:15 -0500

    Navbar with Layout Route

--- index.jsx
+++ index.jsx
@@ -5,25 +5,32 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from './pages/Vans';
 import VanDetail from './pages/VanDetail';
+import Layout from './components/Layout';
 import "./server"
 
 
+
 function App() {
+
+    /**
+   * Challenge: set up the code so we can use a layout route!
+   * 
+   * 1. Create a folder called "components"
+   * 2. Create 2 new component files: Layout.jsx and Header.jsx
+   * 3. Move the <header> code below to the Header component file. 
+   *    (DON'T import that Header component here!)
+   */
+
     return (
         <BrowserRouter>
-            <header>
-                <Link className="site-logo" to="/">#VanLife</Link>
-                <nav>
-                    <Link to="/about">About</Link>
-                    <Link to={"/vans"}>Vans</Link>
 
-                </nav>
-            </header>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/vans" element={<Vans />} />
-                <Route path="/vans/:id" element={<VanDetail />} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/vans" element={<Vans />} />
+                    <Route path="/vans/:id" element={<VanDetail />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
