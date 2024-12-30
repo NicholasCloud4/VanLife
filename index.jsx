15884e932c6128a17b00387f17cafa1f93b1e26c
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-30 16:19:57 -0500

    Bootstrap the Host pages

--- index.jsx
+++ index.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from './pages/Vans';
-import VanDetail from './pages/VanDetail';
+import Home from "./pages/Vans/Home"
+import About from "./pages/Vans/About"
+import Vans from './pages/Vans/Vans';
+import VanDetail from './pages/Vans/VanDetail';
 import Layout from './components/Layout';
+import Dashboard from './pages/Host/Dashboard';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
+
+
 import "./server"
 
+/**
+ * Challenge:
+ * 1. Add a "Host" link to the Navbar that takes you to the "/host" path
+ * 2. Create the following components in the pages/Host folder:
+ *    a. Dashboard ("/host")
+ *    b. Income ("/host/income")
+ *    c. Reviews ("/host/reviews")
+ *    These components can just have an h1 for now that says, e.g.
+ *    "Host Dashboard here".
+ * 3. Set up routes for each of these pages in the Routes below. FOR NOW,
+ *    don't worry about nesting anything, you can just put them on the same
+ *    level as the "/vans", etc. routes below.
+ */
 
 
 function App() {
-
-    /**
-   * Challenge: set up the code so we can use a layout route!
-   * 
-   * 1. Create a folder called "components"
-   * 2. Create 2 new component files: Layout.jsx and Header.jsx
-   * 3. Move the <header> code below to the Header component file. 
-   *    (DON'T import that Header component here!)
-   */
 
     return (
         <BrowserRouter>
@@ -30,6 +39,9 @@ function App() {
                     <Route path="/about" element={<About />} />
                     <Route path="/vans" element={<Vans />} />
                     <Route path="/vans/:id" element={<VanDetail />} />
+                    <Route path='/host' element={<Dashboard />} />
+                    <Route path='/host/income' element={<Income />} />
+                    <Route path='/host/reviews' element={<Reviews />} />
                 </Route>
             </Routes>
         </BrowserRouter>
