be856cc27fcd293c8f07ce3734122e03bbb9eed5
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-02 13:23:39 -0500

    Adding host vans routes

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,20 +1,7 @@
 import React from 'react'
 import { Link, NavLink, Outlet } from 'react-router-dom'
 
-/**
- * Challenge - part 2:
- * Make the host navbar indicate the currently-active route.
- * 
- * Use the following CSS rules:
- *      font-weight: bold;
- *      text-decoration: underline;
- *      color: #161616;
- * 
- * I'd recommend using an inline style this time.
- * 
- * NOTE: There will be a small bug that we'll fix 
- * after you do the challenge.
- */
+
 
 export default function HostLayout() {
 
@@ -29,6 +16,7 @@ export default function HostLayout() {
             <nav className="host-nav">
                 <NavLink to="/host" end style={({ isActive }) => isActive ? activeStyle : undefined}>Dashboard</NavLink>
                 <NavLink to="/host/income" style={({ isActive }) => isActive ? activeStyle : undefined}>Income</NavLink>
+                <NavLink to="/host/vans" style={({ isActive }) => isActive ? activeStyle : undefined}>Vans</NavLink>
                 <NavLink to="/host/reviews" style={({ isActive }) => isActive ? activeStyle : undefined}>Reviews</NavLink>
             </nav>
             <Outlet />
--- index.jsx
+++ index.jsx
@@ -13,9 +13,25 @@ import Reviews from './pages/Host/Reviews';
 
 import "./server"
 import HostLayout from './components/HostLayout';
+import HostVans from './pages/Host/HostVans';
+import HostVanDetail from './pages/Host/HostVanDetail';
 
 
 function App() {
+
+    /**
+   * Challenge: add the /host/vans and /host/vans/:id routes, as well
+   * as the "Vans" link in the Host navbar.
+   * 
+   * For now, just create the stubbed-out version of the pages (i.e.
+   * components that just render an <h1>). Don't worry about adding
+   * navigation from /host/vans to /host/vans/:id yet - the link to
+   * /host/vans is enough for now.
+   * 
+   * When deciding whether or not to use nested routes, keep in mind
+   * what will/won't be shared between these two pages. See the Figma
+   * design file (or the screenshots) to help guide your choice.
+   */
 
     return (
         <BrowserRouter>
@@ -30,6 +46,9 @@ function App() {
                         <Route index element={<Dashboard />} />
                         <Route path='income' element={<Income />} />
                         <Route path='reviews' element={<Reviews />} />
+                        <Route path='vans' element={<HostVans />} />
+                        <Route path='vans/:id' element={<HostVanDetail />} />
+
                     </Route>
                 </Route>
 
