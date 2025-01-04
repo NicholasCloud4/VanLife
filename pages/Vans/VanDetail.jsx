d6b0fdd96d49520dd9fc8657558163d6af85e074
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-04 15:14:08 -0500

    Back to all vans

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 export default function VanDetail() {
 
@@ -26,6 +26,11 @@ export default function VanDetail() {
 
     return (
         <div className="van-detail-container">
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
