74fc72f8bb868ee1c848d83a409051c1005b980f
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-06 19:23:14 -0500

    Updated the way to fetch data from Vans

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
+import { getVans } from '../../api'
 
 
 export default function Vans() {
@@ -11,9 +12,13 @@ export default function Vans() {
 
 
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
+        async function loadVans() {
+            const data = await getVans()
+            setVans(data)
+        }
+
+        loadVans()
+
     }, [])
 
     const displayedVans = typeFilter ? vans.filter(van => van.type === typeFilter) : vans
