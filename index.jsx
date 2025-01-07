e5ff4a332b29174a80fce4af8da2ac0b28d95fbe
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-07 11:53:41 -0500

    Fixed file location

--- index.jsx
+++ index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Vans/Home"
-import About from "./pages/Vans/About"
+import Home from "./pages/Home"
+import About from "./pages/About"
 import Vans from './pages/Vans/Vans';
 import VanDetail from './pages/Vans/VanDetail';
 import Layout from './components/Layout';
