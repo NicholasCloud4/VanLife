3c8d0213d6b287d9c99ad3ee817f437782d6147b
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-07 18:49:50 -0500

    Authentication setup - sad path

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -3,29 +3,44 @@ import { useNavigate, useLocation } from "react-router-dom"
 import { loginUser } from "../api"
 
 /**
- * Challenge: hook up our form so it (halfway) works.
- * 
- * 1. Pull in the `loginUser` function from the api.js file
- * 2. Call loginUser when the form is submitted and log the
- *    data that comes back. Use "b@b.com" as the username and
- *    "p123" as the password.
- * 
- *    NOTE: loginUser returns a promise, so you'll need
- *    a .then(data => {...}) to access the data, or use
- *    a separate aync function defined inside handleSubmit
- * 3. TBA
+ * Challenge: Code the sad path 🙁
+ * 3. Add a `status` state and default it to "idle". When the 
+ *    login form begins submitting, set it to "submitting". When
+ *    it's done submitting and the data has come back, set it 
+ *    to "idle" again
+ * 4. Disable the button when the `status` state is "submitting"
+ *    (this may require some Googling if you haven't done this
+ *    before).
+ * 5. Add an `error` state and default it to `null`. When the
+ *    form is submitted, reset the errors to `null`. If there's
+ *    an error from `loginUser` (add a .catch(err => {...}) in 
+ *    the promise chain), set the error to the error that 
+ *    comes back.
+ * 6. Display the error.message below the `h1` if there's ever
+ *    an error in state
  */
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
     const location = useLocation()
     console.log(location)
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
         loginUser(loginFormData)
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
     }
 
     function handleChange(e) {
@@ -38,8 +53,12 @@ export default function Login() {
 
     return (
         <div className="login-container">
-            {location.state?.message && <h3 className="login-first">{location.state.message}</h3>}
+            {location.state?.message && <h3 className="login-error">{location.state.message}</h3>}
             <h1>Sign in to your account</h1>
+            {
+                error?.message &&
+                <h3 className="login-error">{error.message}</h3>
+            }
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -55,7 +74,9 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button disabled={status === "submitting"} >
+                    {status === "submitting" ? "Logging in..." : "Log in"}
+                </button>
             </form>
         </div>
     )
