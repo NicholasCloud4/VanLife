767093154be988e675bab7f0b9737a2066452402
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-06 19:08:42 -0500

    created 404 page

--- index.jsx
+++ index.jsx
@@ -17,30 +17,21 @@ import HostVanPhotos from './pages/Host/HostVanPhotos';
 
 import "./server"
 import HostVanInfo from './pages/Host/HostVanInfo';
+import NotFound from './pages/NotFound';
 
 
+/**
+ * Challenge: Create a 404 page.
+ * 
+ * 1. Create a new component in the pages dir called "NotFound"
+ * 2. Add the elements from the design. Style it if you want.
+ * 3. Add a "catch-all" route as a nested route under the Route. 
+ *    (It doesn't matter where amongst the children it is.)
+ * 4. Use the NotFound component as the element for that catch-all route
+ */
 
 
 function App() {
-
-    /**
-   * Challenge: Add the routes necessary so we can access
-   * /host/vans/:id/pricing and /host/vans/:id/photos.
-   * 
-   * Add stubbed-out components in separate files for
-   * these routes (e.g. <h2>Pricing view here</h2>). I already
-   * made the `HostVanInfo.jsx`, `HostVanPricing.jsx` and
-   * `HostVanPhotos.jsx` files for you, but they're empty.
-   * 
-   * Don't forget: you'll need to use a special tool from
-   * React Router so we can keep the top info (and 
-   * eventually the navbar we build) on the page while going
-   * from nested route to nested route. This will require some
-   * slight changes to HostVanDetail.jsx
-   * 
-   * Since we don't have the navbar yet, you can test them
-   * by manually navigating to e.g. /host/vans/1/pricing.
-   */
 
     return (
         <BrowserRouter>
@@ -61,10 +52,9 @@ function App() {
                             <Route path='pricing' element={<HostVanPricing />} />
                             <Route path='photos' element={<HostVanPhotos />} />
                         </Route>
-
                     </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Route>
-
             </Routes>
         </BrowserRouter>
     )
